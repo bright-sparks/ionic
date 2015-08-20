28d69622f4384450c619d987f593c1a8dee2d659
Bengt Weiße <bengtler@gmail.com>
2015-08-20 20:39:13 +0200

    Update slideBox.js
    corrected documentation for slide-interval

--- js/angular/directive/slideBox.js
+++ js/angular/directive/slideBox.js
@@ -29,7 +29,7 @@
  * with {@link ionic.service:$ionicSlideBoxDelegate}.
  * @param {boolean=} does-continue Whether the slide box should loop.
  * @param {boolean=} auto-play Whether the slide box should automatically slide. Default true if does-continue is true.
- * @param {number=} slide-interval How many milliseconds to wait to change slides (if does-continue is true). Defaults to 4000.
+ * @param {number=} slide-interval How many milliseconds to wait to change slides (if auto-play is true). Defaults to 4000.
  * @param {boolean=} show-pager Whether a pager should be shown for this slide box. Accepts expressions via `show-pager="{{shouldShow()}}"`. Defaults to true.
  * @param {expression=} pager-click Expression to call when a pager is clicked (if show-pager is true). Is passed the 'index' variable.
  * @param {expression=} on-slide-changed Expression called whenever the slide is changed.  Is passed an '$index' variable.
